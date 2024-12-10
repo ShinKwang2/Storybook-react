bf413345a771d3ca612d6459ae170bfd62313ddc
kwang <shinkwang@yonsei.ac.kr>
2024-12-10 09:39:07 +0900

    Organ: Counter with small component

--- cdd-counter/src/components/atoms/Button.stories.tsx
+++ cdd-counter/src/components/atoms/Button.stories.tsx
@@ -1,9 +1,14 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+
 import Button from './Button';
 
 const meta = {
   title: 'Atoms/Button',
   component: Button,
+  args: {
+    onClick: fn(),
+  },
 } satisfies Meta<typeof Button>;
 
 export default meta;
