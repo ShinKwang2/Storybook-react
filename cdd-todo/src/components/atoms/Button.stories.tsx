0a4bdae6fbe5d06b7be29d3e956b5ba7b186cc37
kwang <shinkwang@yonsei.ac.kr>
2024-12-11 08:42:04 +0900

    feat-atoms: Button component

---
+++ cdd-todo/src/components/atoms/Button.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+import Button from './Button';
+
+const meta = {
+  title: 'Atoms/Button',
+  component: Button,
+  args: {
+    onClick: fn(),
+  },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const RedButton: Story = {
+  args: {
+    label: '삭제',
+  },
+};
+export const BlueButton: Story = {
+  args: {
+    label: '추가',
+    color: '#304ffe',
+  },
+};
---
+++ cdd-todo/src/components/atoms/Button.tsx
@@ -0,0 +1,36 @@
+import styled from '@emotion/styled';
+
+type ContainerProps = {
+  readonly color: string;
+};
+type BUttonProps = {
+  readonly label: string;
+  readonly color?: string;
+  readonly onClick?: () => void;
+};
+
+const Container = styled.button<ContainerProps>`
+  border: 0;
+  color: #ffffff;
+  background-color: ${(props) => props.color};
+  cursor: pointer;
+  padding: 8px 16px;
+  border-radius: 4px;
+
+  &:hover {
+    background-color: ${(props) => props.color};
+    opacity: 0.8;
+  }
+
+  &:active {
+    box-shadow: inset 5px 5px 10px rgba(0, 0, 0, 0.2);
+  }
+`;
+
+export default function Button({ label, color = '#ff5722', onClick }: BUttonProps) {
+  return (
+    <Container color={color} onClick={onClick}>
+      {label}
+    </Container>
+  );
+}
