b723d2fa6805ae0474910172350e4472137c6eb7
kwang <shinkwang@yonsei.ac.kr>
2024-12-10 16:18:43 +0900

    feat-atoms: AppTitle component

---
+++ cdd-todo/src/components/atoms/AppTitle.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { BrowserRouter } from 'react-router-dom';
+import AppTitle from './AppTitle';
+
+const meta = {
+  title: 'Atoms/AppTitle',
+  component: AppTitle,
+  parameters: {
+    backgrounds: {
+      default: 'Header background color',
+      values: [{ name: 'Header background color', value: '#304ffe' }],
+    },
+  },
+} satisfies Meta<typeof AppTitle>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: () => (
+    <BrowserRouter>
+      <AppTitle />
+    </BrowserRouter>
+  ),
+};
