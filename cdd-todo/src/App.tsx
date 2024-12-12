f4d36d0a842a3a294f0d67fe3c9f4edac1cf41c7
kwang <shinkwang@yonsei.ac.kr>
2024-12-12 15:51:20 +0900

    conclusion: change component in  App.tsx

--- cdd-todo/src/App.tsx
+++ cdd-todo/src/App.tsx
@@ -1,10 +1,22 @@
-import "./App.css";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { TodoListContextProvider } from './contexts/TodoList-context';
+import Header from './components/organisms/Header';
+import TodoListPage from './pages/TodoListPage';
+import TodoInputPage from './pages/TodoInputPage';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <>
-      <div>앱</div>
-    </>
+    <BrowserRouter>
+      <TodoListContextProvider>
+        <Header />
+        <Routes>
+          <Route path="/" element={<TodoListPage />} />
+          <Route path="/add" element={<TodoInputPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </TodoListContextProvider>
+    </BrowserRouter>
   );
 }
 
